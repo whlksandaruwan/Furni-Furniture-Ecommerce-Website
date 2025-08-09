bce512e6b26ba136fdf7b49cc86913ac67bec036
Kavindu Sandaruwan <147842428+whlksandaruwan@users.noreply.github.com>
2025-08-09 21:04:30 +0530

    Remove Backend and Make Frontend Standalone

--- backend/src/config/jwt.config.ts
+++
@@ -1,9 +0,0 @@
-import { JwtModuleOptions } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-
-export const getJwtConfig = (configService: ConfigService): JwtModuleOptions => ({
-  secret: configService.get('JWT_SECRET'),
-  signOptions: {
-    expiresIn: configService.get('JWT_EXPIRES_IN'),
-  },
-}); 
