a42d94b9115d0668c2ae61a8d34a912b84c1f5eb
whlksandaruwan <whlksandaruwan@students.nsbm.ac.lk>
2025-08-12 22:16:47 +0530

    update for deploye

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   base: '/furni-furniture-ecommerce/',
+  publicDir: 'public',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -14,5 +15,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 })
